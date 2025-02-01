112584bf8dd4fb32fb0dc8cebcb6d1380c3cd878
pepi1401 <pptarnovo1401@gmail.com>
2025-02-01 19:10:04 +0100

    Add dynamic details link for each movie

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,13 @@
 export default [
     {
+        id: 1,
         imageUrl: '/static/img/jungle-cruise.jpeg',
         title: 'Jungle Cuise',
         genre: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
     },
     {
+        id: 2,
         imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTk5ODk1NDkxMF5BMl5BanBnXkFtZTcwNTA5OTY0OQ@@._V1_FMjpg_UX1000_.jpg',
         title: 'Man of Steel',
         genre: 'Superhero',
