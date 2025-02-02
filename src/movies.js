07f489fe5f9dea5b303d1d2ea98310bb21c506f0
pepi1401 <pptarnovo1401@gmail.com>
2025-02-02 13:57:37 +0100

    Add create movie
    add uuid

--- src/movies.js
+++ src/movies.js
@@ -1,24 +1,35 @@
 export default [
     {
-        id: '1',
+        id: 'ba874c89-3384-4f1b-af0b-2603930e1392',
         imageUrl: '/static/img/jungle-cruise.jpeg',
         title: 'Jungle Cuise',
         genre: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
         director: 'Jaume Collet-Serra',
-        year: 2021,
-        rating: 6.6,
+        year: '2021',
+        rating: '6.6',
         category: 'movie',
     },
     {
-        id: '2',
+        id: 'tu809v45-3384-4f1b-af0b-2603930m5609',
         imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTk5ODk1NDkxMF5BMl5BanBnXkFtZTcwNTA5OTY0OQ@@._V1_FMjpg_UX1000_.jpg',
         title: 'Man of Steel',
         genre: 'Superhero',
         description: 'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.',
         director: 'Zack Snyder',
-        year: 2013,
-        rating: 7.1,
+        year: '2013',
+        rating: '7.1',
         category: 'movie',
-    }
+    },
+    {
+        id: '68cc5b07-ae3c-4015-b922-37adb095f256',
+        title: 'Avengers: Endgame',
+        category: 'movie',
+        genre: 'Superhero',
+        director: 'Anthony Russo',
+        year: '2019',
+        imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_FMjpg_UX1000_.jpg',
+        rating: '8.4',
+        description: "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe."
+      }
 ]
